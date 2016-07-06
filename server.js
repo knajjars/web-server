2458e17a0c162f86b2b1ef307efe28c2fba294cc
Khalil Najjar <knajjars@gmail.com>
2016-07-06 12:50:02 -0600

    Port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-const PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000;
 var middleware = require('./middleware');
 
 app.use(middleware.logger);
