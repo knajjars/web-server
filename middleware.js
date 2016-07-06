9f729d81492abcbf5682f42b4452a8bc37d1a77e
Khalil Najjar <knajjars@gmail.com>
2016-07-06 12:56:15 -0600

    cambios

--- middleware.js
+++ middleware.js
@@ -1,13 +1,14 @@
-/**
- * Created by khalilnajjar on 6/7/16.
- */
-module.exports = {
-    requireAuthentication: function (req, res, next) {
-        console.log('private route hit!');
-        next();
-    },
-    logger: function (req, res, next) {
-        console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
-        next();
-    }
+// Use module exports to expose middleware
+
+var middleware = {
+	requireAuthentication: function (req, res, next) {
+		console.log('private route hit!');
+		next();
+	},
+	logger: function (req, res, next) {
+		console.log('Request: ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
+		next();
+	}
 };
+
+module.exports = middleware;
--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
-var middleware = require('./middleware');
+
+var middleware = require('./middleware.js');
 
 app.use(middleware.logger);
 
-app.get('/about', middleware.requireAuthentication ,function(req, res) {
-    res.send('About us!');
+app.get('/about', middleware.requireAuthentication, function (req, res) {
+	res.send('About Us!');
 });
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(3000, function() {
-    console.log('Server listening on port ' + PORT);
+app.listen(PORT, function () {
+	console.log('Express server started on port ' + PORT + '!');
 });
